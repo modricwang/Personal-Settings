3d71f8b9d530c3ff4052e54c7cb00d6dae8eea55
Modric Wang <modricwang@126.com>
2017-11-22 00:23:49 +0800

    update git-proxy-ss
    引号和前缀都要去掉，否则lfs会报错

--- proxy/git-proxy-ss.bat
+++ proxy/git-proxy-ss.bat
@@ -1,3 +1,3 @@
 git config --global http.postBuffer 524288000
-git config --global http.proxy 'socks5://127.0.0.1:1080'
-git config --global https.proxy 'socks5://127.0.0.1:1080'
+git config --global http.proxy 127.0.0.1:1080
+git config --global https.proxy 127.0.0.1:1080
